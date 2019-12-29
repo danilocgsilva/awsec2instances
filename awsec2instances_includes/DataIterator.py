9fe88a984085e8fde63a0b1f946378fcd0ea7049
Danilo Silva <danilocgsilva@gmail.com>
2019-12-28 23:23:13 -0300

    Refactor for DataIterator to allow future tests

--- awsec2instances_includes/DataIterator.py
+++ awsec2instances_includes/DataIterator.py
@@ -9,24 +9,29 @@ from awsec2instances_includes.fn import \
 class DataIterator:
 
     def showInstancesInfos(self, instances):
-
-        loopInteration = 0
+        iteration_loop = 1
         for instance in instances:
-
             instanceInfos = instance["Instances"][0]
-            state = extracState( instanceInfos )
+            array_data = self.__get_data_to_print__(instanceInfos)
+            self.__print_data_single_instance__(array_data, iteration_loop)
+            iteration_loop += 1
 
-            loopInteration += 1
-            tipoInstancia = extractInstanceType( instanceInfos )
-            instanceId = extractInstanceId( instanceInfos )
-            enderecoInstancia = extractPublicIpAddress( instanceInfos )
-            identificacao = extractName( instanceInfos )
-            
 
-            print('Instance count:', loopInteration)
-            print('Instance Id:', instanceId)
-            print('Instance type:', tipoInstancia)
-            print('PublicIp:', enderecoInstancia)
-            print('Name:', identificacao)
-            print('Status:', state)
-            print('---')
+    def __get_data_to_print__(self, instanceInfos):
+        state = extracState( instanceInfos )
+        tipoInstancia = extractInstanceType( instanceInfos )
+        instanceId = extractInstanceId( instanceInfos )
+        enderecoInstancia = extractPublicIpAddress( instanceInfos )
+        identificacao = extractName( instanceInfos )
+
+        return [instanceId, tipoInstancia, enderecoInstancia, identificacao, state]
+
+
+    def __print_data_single_instance__(self, list_data: list, iteration_loop: int):
+        print('Instance counted:',  iteration_loop)
+        print('Instance Id:', list_data[0])
+        print('Instance type:', list_data[1])
+        print('PublicIp:', list_data[2])
+        print('Name:', list_data[3])
+        print('Status:', list_data[4])
+        print('---')

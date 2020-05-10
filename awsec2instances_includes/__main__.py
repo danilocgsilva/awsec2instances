e4ea78e0294a9d8cd70080ed498c05c2224ff3ad
Danilo Silva <danilocgsilva@gmail.com>
2020-05-10 20:51:54 -0300

    Setup added
    Now you can use pip to make the local installation.

---
+++ awsec2instances_includes/__main__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from awsec2instances_includes.DataIterator import DataIterator
+from awsec2instances_includes.fn import getRawDataFromCli
+from awsec2instances_includes.Talk import Talk
+import sys
+
+def main():
+    instances = getRawDataFromCli()
+
+    di = DataIterator()
+    data_instances = di.getInstancesInfos(instances)
+
+    talk = Talk()
+    talk.get_instance_data(data_instances)
+    talk.printData()
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+VERSION = '1.0.0'
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+setup(
+    name="awsec2instances",
+    version=VERSION,
+    description="Wrapper around the aws command line",
+    long_description_content_type="text/markdown",
+    long_description=readme(),
+    keywords="aws ec2 list command line",
+    url="https://github.com/danilocgsilva/awsec2instances",
+    author="Danilo Silva",
+    author_email="contact@danilocgsilva.me",
+    packages=["awsec2instances_includes"],
+    entry_points={"console_scripts": ["awsec2=awsec2instances_includes.__main__:main"],},
+    includes_package_data=True
+)

734f6ae140323d8a282e6355bb0017e6bc0536fc
Danilo Silva <contact@danilocgsilva.me>
2020-09-12 19:47:28 -0300

    Starting better Resume class

--- awsec2instances_includes/Resume.py
+++ awsec2instances_includes/Resume.py
@@ -3,14 +3,13 @@ class Resume:
 
     def __init__(self):
         self.instances_data = []
+        self.instanceCount = 0
 
     def add_instances_data(self, data: dict):
         self.instances_data.append(data)
         return self
 
     def get_instance_count(self):
-        instance_cout = 0
         for instance_data in self.instances_data:
-            instance_cout = instance_cout + len(instance_data)
-        return instance_cout
-
+            self.instanceCount = self.instanceCount + len(instance_data)
+        return self.instanceCount
--- tests/test_Resume.py
+++ tests/test_Resume.py
@@ -2,6 +2,7 @@ import unittest
 import sys
 sys.path.insert(1, "..")
 from awsec2instances_includes.Resume import Resume
+from awsapimock.Instance_Data_Generator import Instance_Data_Generator
 
 
 class test_Resume(unittest.TestCase):
@@ -9,5 +10,9 @@ class test_Resume(unittest.TestCase):
     def setUp(self):
         self.resume = Resume()
 
-    def test_add_instances_data(self):
-        
+    def test_add_instances_data__get_instance_count(self):
+        instance_data_sample = Instance_Data_Generator().generate()
+        self.resume.add_instances_data(instance_data_sample)
+        counting_instances = 1
+        returned_instances = self.resume.get_instance_count()
+        self.assertEqual(counting_instances, returned_instances)

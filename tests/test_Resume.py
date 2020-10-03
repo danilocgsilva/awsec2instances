494b2aa2f718b222dfb82cc94e5f54df7e594a18
Danilo Silva <contact@danilocgsilva.me>
2020-10-03 06:38:34 -0300

    Update package dependency for tests

--- tests/test_Resume.py
+++ tests/test_Resume.py
@@ -2,7 +2,7 @@ import unittest
 import sys
 sys.path.insert(1, "..")
 from awsec2instances_includes.Resume import Resume
-from awsapimock.Instance_Data_Generator import Instance_Data_Generator
+from awsapimock.Instance_Request_Generator import Instance_Request_Generator
 
 
 class test_Resume(unittest.TestCase):
@@ -11,7 +11,7 @@ class test_Resume(unittest.TestCase):
         self.resume = Resume()
 
     def test_add_instances_data__get_instance_count(self):
-        instance_data_sample = Instance_Data_Generator().generate()
+        instance_data_sample = Instance_Request_Generator().generate()
         self.resume.add_instances_data(instance_data_sample)
         counting_instances = 1
         returned_instances = self.resume.get_instance_count()

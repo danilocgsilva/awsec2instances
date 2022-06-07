efcfe76570a0f7e82a50d93785199ba9ce19d127
Danilo Carlos <danilo.carlos@mobly.com.br>
2022-06-07 15:17:53 -0300

    Allow external database access

--- awsec2instances_includes/DatabaseProcess/DatabaseProcessUbuntu.py
+++ awsec2instances_includes/DatabaseProcess/DatabaseProcessUbuntu.py
@@ -9,6 +9,10 @@ class DatabaseProcessUbuntu(DatabaseProcessInterface):
         protocolService.ensure_port_3306()
 
         userScript.add_scripts("apt install mariadb-server mariadb-client -y")
+        userScript.add_scripts(self.__bind_script())
         userScript.add_scripts("systemctl enable --now mariadb")
-        
+
         return self
+
+    def __bind_script(self) -> str:
+        return "sed -i /bind-address/s/127.0.0.1/0.0.0.0/g /etc/mysql/mariadb.conf.d/50-server.cnf"

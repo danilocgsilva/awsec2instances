9af6549e6e7337839c5d5215b466513d832d48b6
Danilo Silva <contato@danilocgsilva.me>
2022-09-11 21:12:57 -0300

    Warns the instance lifetime in creation

--- awsec2instances_includes/CreationInstanceService.py
+++ awsec2instances_includes/CreationInstanceService.py
@@ -1,12 +1,14 @@
 from awsec2instances_includes.UserScript import UserScript
 from awsec2instances_includes.ProtocolService import ProtocolService
 from awsec2instances_includes.UserScript import UserScript
+import re
 
 class CreationInstanceService:
 
     def __init__(self):
         self.needs_die_warnning = True
         self.die_time = 5
+        self.lifetime_human_communicate = "five minutes"
 
     def getCreationServices(self, accesses):
         '''
@@ -18,6 +20,16 @@ class CreationInstanceService:
         userScript.add_scripts("shutdown -P +" + str(self.die_time))
 
     def ensureMinutesData(self, human_given_parameter = None):
+        """
+        The *human_given_parameter* is a string or a number.
+        If the given value can be converted to an int, so the application
+          understands that the value is the value in minutos giving the
+          instance lifetime. Otherwise, this trials throws an exception, 
+          which is treated as an expected string to drive the lifetime in
+          a more understadable way, instead of just giving a number, and
+          this string then is converted to the real time in minutes that
+          the instance will take alive.
+        """
         if not human_given_parameter:
             return
         self.needs_die_warnning = False
@@ -40,7 +52,14 @@ class CreationInstanceService:
                 self.die_time = 2102424
             else:
                 raise Exception("The parameter given is not known.")
-
+            self.lifetime_human_communicate = human_given_parameter
+            
+    def get_lifetime(self):
+        if self.lifetime_human_communicate != "forever":
+            return re.sub("-", " ", self.lifetime_human_communicate)
+        else:
+            return "four years"
+        
     def getHarakiriMessage(self) -> str:
         message = "Your instance are setted to make an harakiri in just five minutes! Somehing good to avoid unexpected useless instance consuming your money in AWS.\n"
         message += "If you are pretty sure that you will need an instance that must lasts for more time, use the argument \"--lasts\" with a value:\n"
